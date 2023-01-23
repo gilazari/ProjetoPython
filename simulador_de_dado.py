255f7a17cdc37f9b661bc8dcce6cdeb9d075dca1
gilazari <gio_lazari@hotmail.com>
2023-01-23 16:26:22 -0300

    Projeto1

---
+++ simulador_de_dado.py
@@ -0,0 +1,24 @@
+#Simulador de dado
+
+import random
+#import PySimpleGUI
+
+class SimuladorDado:
+    def __init__(self):
+        self.valorminimo = 1
+        self.valormaximo = 6
+    
+    def iniciar(self):
+        resposta = input("Deseja rodar o dado?")
+        if resposta == "sim":
+            self.GerarValorDado()
+        elif resposta == "nao":
+            print("OK")
+        else:
+            print("Pfvr digitar sim ou não")
+
+    def GerarValorDado(self):
+        print(random.randint(self.valorminimo,self.valormaximo))
+
+simulador = SimuladorDado()
+simulador.iniciar() 
